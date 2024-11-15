988d1b82897e42e7012dc3a1c8e0610533226b47
Davinder2001 <162427407+Davinder2001@users.noreply.github.com>
2024-11-15 19:20:26 +0530

    Testimonial almost

--- src/app/_common/testimonial/testimonial.jsx
+++ src/app/_common/testimonial/testimonial.jsx
@@ -1,58 +1,105 @@
 "use client";
-import { useContext } from 'react';
-import Image from 'next/image';
-import { SectorDataContext } from '@/context/apiContext';
+import { useContext, useEffect } from "react";
+import Image from "next/image";
+import { SectorDataContext } from "@/context/apiContext";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { Navigation, Pagination } from "swiper/modules";
 
 const Testimonial = () => {
   const { testimonialsApi } = useContext(SectorDataContext);
-  return (
-    <div className='container'>
-    <div className="testimonials">
-      <div className="testimonial-heading">
-          <h3>What Our Clients Say About Us</h3>
-      </div>
-    <div className="testimonials-inner">
-      {testimonialsApi?.map((section, sectionIndex) => (
-        <div key={sectionIndex} className='testimonial'> 
-        <div className="testimonial-inner">
-          <div className="testimonial-background-img">
-              <img src='/images/Vector5.png' className='test-m-1'/>
-              <img src='/images/Vector6.png'  className='test-m-2'/>
-          </div>
-          <div className="testimonial-frontt">
 
-          <div className="testimonial-image">
-              <Image src={section.acf.image} 
-              alt='img'
-              layout="responsive" 
-              width={100} 
-              height={100} 
-              />
-          </div>
-          <div className="testimonial-name">
-            <h2>{section.acf.name}</h2>
-          </div>
-          <div className="testimonial-desg">
-              <p>{section.acf.designation}</p>
-          </div>
-            <div className="qutee-img">
-              <Image 
-                src="/images/clarity_block-quote-line.png"
-                alt="icon"
-                width={40} // Adjust width
-                height={40} // Adjust height to make it square
-                layout="intrinsic" // Use intrinsic layout for icon-style images
-              />
-            </div>
-            <div className="testimonial-main-text">
-              <p>{section.acf.description}</p>
-            </div>
-          </div>
+  useEffect(() => {
+    const initSwiper = () => {
+      const prevButton = document.querySelector(".custom-prev");
+      const nextButton = document.querySelector(".custom-next");
+
+      if (prevButton && nextButton) {
+        const swiper = document.querySelector(".testimonials-inner")?.swiper;
+        if (swiper) {
+          swiper.params.navigation.prevEl = prevButton;
+          swiper.params.navigation.nextEl = nextButton;
+          swiper.navigation.init();
+          swiper.navigation.update();
+        }
+      }
+    };
+
+    initSwiper();
+  }, []);
+
+  return (
+    <div className="container">
+      <div className="testimonials">
+
+        <div className="testimonials-pagination">
+        <div className="testimonial-heading">
+          <button className="custom-prev">←</button>
+          <h3>What Our Clients Say About Us</h3>
+          <button className="custom-next">→</button>
         </div>
+        <div className="pagination-top" />
+        </div>
+
+
+        <Swiper
+          modules={[Navigation, Pagination]}
+          spaceBetween={20}
+          slidesPerView={3}
+          pagination={{
+            el: ".pagination-top", // Link to custom top pagination element
+            clickable: true,
+          }}
+          breakpoints={{
+            320: { slidesPerView: 1 },
+            768: { slidesPerView: 2 },
+            1024: { slidesPerView: 3 },
+          }}
+          className="testimonials-inner"
+        >
+          {testimonialsApi?.map((section, sectionIndex) => (
+            <SwiperSlide key={sectionIndex} className="testimonial">
+              <div className="testimonial-inner">
+                <div className="testimonial-background-img">
+                  <img src="/images/Vector5.png" className="test-m-1" />
+                  <img src="/images/Vector6.png" className="test-m-2" />
+                </div>
+                <div className="testimonial-frontt">
+                  <div className="testimonial-image">
+                    <Image
+                      src={section.acf.image}
+                      alt="Testimonial Image"
+                      layout="responsive"
+                      width={100}
+                      height={100}
+                    />
+                  </div>
+                  <div className="testimonial-name">
+                    <h2>{section.acf.name}</h2>
+                  </div>
+                  <div className="testimonial-desg">
+                    <p>{section.acf.designation}</p>
+                  </div>
+                  <div className="qutee-img">
+                    <Image
+                      src="/images/clarity_block-quote-line.png"
+                      alt="Quote Icon"
+                      width={40}
+                      height={40}
+                      layout="intrinsic"
+                    />
+                  </div>
+                  <div className="testimonial-main-text-des">
+                    <p>{section.acf.description}</p>
+                  </div>
+                </div>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
-      ))}
-      </div>
-    </div>
     </div>
   );
 };
--- src/app/about-us/components/aboutUs.jsx
+++ src/app/about-us/components/aboutUs.jsx
@@ -1,86 +1,12 @@
-'use client';
-import { useContext, useState, useEffect } from 'react';
-import Image from 'next/image';
-import { SectorDataContext } from '@/context/apiContext';
+import React from 'react'
+import AboutUsComp from '@/app/_common/aboutUs/aboutUsComp'
 
 const AboutUsMain = () => {
-    const pagesDataApi = useContext(SectorDataContext);
-    const mainData = pagesDataApi?.pagesDataApi?.find(page => page.slug === 'about-us')?.acf?.third_section;
+  return (
+    <>
+        <AboutUsComp/>
+    </>
+  )
+}
 
-    // Set the initial main image to the first image in the gallery on component load
-    const [mainImage, setMainImage] = useState(null);
-
-    // Ensure the first image is set as the main image on initial render
-    useEffect(() => {
-        if (mainData?.image_gallery?.length > 0) {
-            setMainImage(mainData.image_gallery[0].images);
-        }
-    }, [mainData]);
-
-    const handleThumbnailClick = (newMainImage) => {
-        setMainImage(newMainImage);
-    };
-
-    return (
-        <div className="about-us-main container">
-            {mainData && (
-                <div className='first-section-inner'>
-                    <div className="page-title">
-                        <h1>{mainData.slider_text}</h1>
-                    </div>
-                    <div className="short-description">
-                        <p>{mainData.small_description}</p>
-                    </div>
-                    
-                <div className='image-gallery-container'>
-                    {/* Display the main image */}
-                    {mainImage && (
-                        <div className="top-big-image">
-                            <Image 
-                                src={mainImage} 
-                                alt="Main Image"
-                                layout="responsive" 
-                                width={100} 
-                                height={100} 
-                            />
-                        </div>
-                    )}
-
-                    <div className="description-column-main">
-                        <div className="page-heading background">
-                            <h2>{mainData.heading}</h2>
-                        </div>
-                        <div className='main-description'>
-                            <p>{mainData.main_description}</p>
-                        </div>
-                    
-                    <div className="image-gallery inline-thumbnails">
-                        {mainData.image_gallery?.map((image, index) => (
-                            <div 
-                                key={index} 
-                                className={`thumbnail ${mainImage === image.images ? 'active' : ''}`} 
-                                onClick={() => handleThumbnailClick(image.images)}
-                            >
-                                <Image  
-                                    src={image.images} 
-                                    alt={`Thumbnail ${index + 1}`}
-                                    layout="fixed" 
-                                    width={100} 
-                                    height={100} 
-                                    className="thumbnail-image"
-                                />
-                            <div className="overlay"></div>
-                            </div>
-                            
-                        ))}
-                        </div>
-                    </div>
-                </div>
-                    
-                </div>
-            )}
-        </div>
-    );
-};
-
-export default AboutUsMain;
+export default AboutUsMain
