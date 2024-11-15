ad3dcebaab531e981ca2d95a5dc0a708c99202c9
Davinder2001 <162427407+Davinder2001@users.noreply.github.com>
2024-11-15 17:19:34 +0530

    Testimonial Changes

--- src/app/_common/testimonial/testimonial.jsx
+++ src/app/_common/testimonial/testimonial.jsx
@@ -7,12 +7,20 @@ const Testimonial = () => {
   const { testimonialsApi } = useContext(SectorDataContext);
   return (
     <div className='container'>
-    <h3>What Our Clients Say About Us</h3>
     <div className="testimonials">
+      <div className="testimonial-heading">
+          <h3>What Our Clients Say About Us</h3>
+      </div>
     <div className="testimonials-inner">
       {testimonialsApi?.map((section, sectionIndex) => (
         <div key={sectionIndex} className='testimonial'> 
         <div className="testimonial-inner">
+          <div className="testimonial-background-img">
+              <img src='/images/Vector5.png' className='test-m-1'/>
+              <img src='/images/Vector6.png'  className='test-m-2'/>
+          </div>
+          <div className="testimonial-frontt">
+
           <div className="testimonial-image">
               <Image src={section.acf.image} 
               alt='img'
@@ -21,17 +29,27 @@ const Testimonial = () => {
               height={100} 
               />
           </div>
+          <div className="testimonial-name">
             <h2>{section.acf.name}</h2>
-            <h3>{section.acf.designation}</h3>
-            <Image src="/images/clarity_block-quote-line.png"
-            alt='img'
-            layout="responsive" 
-            width={100} 
-            height={100} 
-            />
-            <p>{section.acf.description}</p>
+          </div>
+          <div className="testimonial-desg">
+              <p>{section.acf.designation}</p>
+          </div>
+            <div className="qutee-img">
+              <Image 
+                src="/images/clarity_block-quote-line.png"
+                alt="icon"
+                width={40} // Adjust width
+                height={40} // Adjust height to make it square
+                layout="intrinsic" // Use intrinsic layout for icon-style images
+              />
+            </div>
+            <div className="testimonial-main-text">
+              <p>{section.acf.description}</p>
+            </div>
           </div>
         </div>
+      </div>
       ))}
       </div>
     </div>
