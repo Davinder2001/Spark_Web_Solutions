17aea04b1bb52447e0a9a7d8d91ec3c7d74a719f
raaj73906@gmail.com <neosharma995@gmail.com>
2025-01-22 18:52:47 +0530

    change loader into text

--- src/app/_common/loader/loader.jsx
+++ src/app/_common/loader/loader.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
-import Image from 'next/image';
+// import Image from 'next/image';
 
 const Loader = () => {
   return (
     <div className="loader">
-     <h1 style={{fontSize:'60px'}}>Welcome!  to the Sparkweb solutions</h1>
+      {/* <Image
+        src="/siteLogo/logo.jpg" 
+        alt="Loading..."
+        width={100}
+        height={100}
+        className="loading-img"
+      /> */}
+      <h1 style={{fontSize:'60px'}}>Welcome!  to the Sparkweb solutions</h1>
     </div>
   );
 };
